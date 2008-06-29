db2eef03459fef70d842c52ac895d77896016eab
Ryan Lowe <ryanlowe@gmail.com>
2008-06-29 17:53:58 -0400

    change exception notifier sender email address

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,4 @@
 # ExceptionNotifier plugin configuration
 ExceptionNotifier.exception_recipients = %w(ryanlowe@gmail.com)
-ExceptionNotifier.sender_address = %("Listens Exception")
+ExceptionNotifier.sender_address = %("Listens Exception" <db4442e2@db4442e2.fb.joyent.us>)
 ExceptionNotifier.email_prefix = ""
