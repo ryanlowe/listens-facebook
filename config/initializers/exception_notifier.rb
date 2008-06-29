b39b2b1002774521267b5f41910b42088f742523
Ryan Lowe <ryanlowe@gmail.com>
2008-06-29 17:51:51 -0400

    change exception notifier sender email address

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,4 +1,4 @@
 # ExceptionNotifier plugin configuration
 ExceptionNotifier.exception_recipients = %w(ryanlowe@gmail.com)
-ExceptionNotifier.sender_address = %("Listens Exception" <exception@audioman.org>)
-ExceptionNotifier.email_prefix = "[Listens ERROR] "
+ExceptionNotifier.sender_address = %("Listens Exception")
+ExceptionNotifier.email_prefix = ""
